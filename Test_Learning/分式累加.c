53df3099de5ed35a3bac3742a5f1b56d0e435528
chinohao <haohao927a@outlook.com>
2024-09-26 23:53:06 +0800

    commit message: ``` 优化素数判断函数和数学序列求值方法
    - 素数判断：重构原有的素数判断逻辑，通过专门的函数提高代码的可读性和复用性。
    - 数学序列：优化数学序列的求值方法，使用更高效的算法减少计算复杂度，提升执行效率。
    
    重构代码结构，提高代码可读性和执行效率。
    ```

--- Test_Learning/分式累加.c
+++ Test_Learning/分式累加.c
@@ -2,15 +2,23 @@
 
 int main () {
     int n;
-    double result = 1;
+    double result = 1, a1 = 1, b1 = 2, c1, a0 = 1, b0 = 1, c0;
     scanf("%d", &n);
-    for (int i = 2; i <= n; i++) {
-        double j = i;
-        if (i % 2 == 0) {
-            result -= (j - 1) / (j);
-        }
-        else if (i % 2 != 0) {
-            result += (j - 1) / (j);
+    if (n >= 2) {
+        result -= 0.5;
+        for (int i = 3; i <= n; i++) {
+            c1 = a1 + b1;
+            c0 = a0 + b0;
+            if (i % 2 == 0) {
+                result -= (c0) / (c1);
+            }
+            else if (i % 2 != 0) {
+                result += (c0) / (c1);
+            }
+            a1 = b1;
+            b1 = c1;
+            a0 = b0;
+            b0 = c0;
         }
     }
     printf("%lf", result);
