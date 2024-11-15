a9350d3dda592ac3ae910a5d0940fe1a14812a7e
ChinoKou <haohao927a@outlook.com>
2024-11-15 05:23:38 +0000

    2024年11月15日 13:23

--- Test_Learning/hw_lab/extra_hw/2/hw7.c
+++ Test_Learning/hw_lab/extra_hw/2/hw7.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int statistics (char arr[], char character);
+void p_statistics(char* arr, char character, int* count);
 
 int main () {
     char arr[] = "f65g6g54gs65waey6854qt465q456gvGUIKQJHB#QUIKJFG1age35a35g1A6G4A65G4A2G1AGARQ635R3Q";
     char character = '3';
     int count = statistics(arr, character);
+    printf("%d", count);
+    p_statistics(arr, character, &count);
     printf("%d", count);
     return 0;
 }
@@ -18,4 +21,13 @@ int statistics (char arr[], char character) {
         }
     }
     return count;
+}
+
+void p_statistics(char* arr, char character, int* count) {
+    *count = 0;
+    for (int i = 0; *(arr + i) != '\0'; i++) {
+        if (*(arr + i) == character) {
+            *count++;
+        }
+    }
 }
