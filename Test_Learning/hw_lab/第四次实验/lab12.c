1f37da0222f7c914e9f9cb7207c51418e57f869e
ChinoKou <haohao927a@outlook.com>
2024-11-15 14:31:26 +0000

    2024年11月15日 22:31

--- Test_Learning/hw_lab/第四次实验/lab12.c
+++ Test_Learning/hw_lab/第四次实验/lab12.c
@@ -54,9 +54,9 @@ double Static (stu ArrStu[]) {
             }
         }
     }
-    printf("学生成绩排序如下: \n");
+    printf("学生成绩排序如下: \n名次\t 姓名\t 学号\t 成绩\t");
     for (int i = 0; i < 10; i++) {
-        printf("%d %s %s %d\n", i, ArrStu[i].name, ArrStu[i].stu_num, ArrStu[i].marks);
+        printf("%d\t %s\t %s\t %lf\t\n", i + 1, ArrStu[i].name, ArrStu[i].stu_num, ArrStu[i].marks);
     }
     for (int i = 0; i < 5; i++) {
         printf("成绩等级为 %c 的有 %d 位同学\n", char_grade[i], grade[i]);
