5d86fa32c057078cde870b7b3f65165856ad8d68
ChinoKou <haohao927a@outlook.com>
2024-11-15 14:19:36 +0000

    2024年11月15日 22:19

--- Test_Learning/hw_lab/第四次实验/lab12.c
+++ Test_Learning/hw_lab/第四次实验/lab12.c
@@ -2,9 +2,63 @@
 
 typedef struct student {
     char name[100];
-    int num;
-    int marks;
+    char stu_num;
+    double marks;
 }stu;
+
+double Static (stu ArrStu[]);
+
 int main () {
-    stu ArrStu[10] = {};
+    stu ArrStu[10];
+    for (int i = 0; i < 10; i++) {
+        printf("\n请输入第 %d 位学生的学号: ", i + 1);
+        scanf("%s", ArrStu[i].stu_num);
+        printf("请输入第 %d 位学生的姓名: ", i + 1);
+        scanf("%s", ArrStu[i].name);
+        printf("请输入第 %d 位学生的分数: ", i + 1);
+        scanf("%lf", ArrStu[i].marks);
+    }
+    double avg = Static(ArrStu);
+    return 0;
+}
+
+double Static (stu ArrStu[]) {
+    int grade[5] = {0, 0, 0, 0, 0};
+    char char_grade[6] = "ABCDE";
+    double sum = 0;
+    for (int i = 0; i < 10; i++) {
+        if (ArrStu[i].marks >= 90) {
+            grade[0]++;
+        }
+        else if (ArrStu[i].marks >= 80 && ArrStu[i].marks < 90) {
+            grade[1]++;
+        }
+        else if (ArrStu[i].marks >= 70 && ArrStu[i].marks < 80) {
+            grade[2]++;
+        }
+        else if (ArrStu[i].marks >= 60 && ArrStu[i].marks < 70) {
+            grade[3]++;
+        }
+        else if (ArrStu[i].marks >= 0 && ArrStu[i].marks < 60) {
+            grade[4]++;
+        }
+        sum += ArrStu[i].marks;
+    }
+    for (int i = 0; i < 10; i++) {
+        for (int j = i; j < 9; j++) {
+            if (ArrStu[i].marks < ArrStu[j].marks) {
+                stu temp = ArrStu[i];
+                ArrStu[i] = ArrStu[j];
+                ArrStu[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < 5; i++) {
+        printf("成绩等级为 %c 的有 %d 位同学\n", char_grade[i], grade[i]);
+    }
+    printf("学生成绩排序如下: \n");
+    for (int i = 0; i < 10; i++) {
+        printf("");
+    }
+    return (sum / 10);
 }
