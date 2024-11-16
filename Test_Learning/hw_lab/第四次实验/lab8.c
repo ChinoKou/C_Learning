fb7b9f6b07600153b8ade1addf93b77b2fdba380
ChinoKou <haohao927a@outlook.com>
2024-11-16 12:22:22 +0000

    2024年11月16日 20:22

--- Test_Learning/hw_lab/第四次实验/lab8.c
+++ Test_Learning/hw_lab/第四次实验/lab8.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+void sort (char* s);
 int main () {
-    char s[1000];
-    scanf("%s", &s);
-    
-    int len = strlen(s);
-    for (int i = 0; i < len; i++) {
-        if (s[i] == '\0') {
-            s[i + 1] += 20;
-        }
+    char s[5][100];
+    for (int i = 0; i < 5; i++) {
+        scanf("%s", s[i]);
     }
+
     printf("%s", s);
     return 0;
+}
+
+void sort (char* s) {
+    
 }
