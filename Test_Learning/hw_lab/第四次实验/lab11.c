8ea115dfbd9aa969dc2f6c6fea115dda88588e9a
ChinoKou <haohao927a@outlook.com>
2024-11-16 10:23:34 +0000

    2024年11月16日 18:23

--- Test_Learning/hw_lab/第四次实验/lab11.c
+++ Test_Learning/hw_lab/第四次实验/lab11.c
@@ -4,6 +4,29 @@ typedef struct {
     int year, month, day;
 } Date;
 
+int FishNet (Date Fish);
+
+
 int main () {
-    
+    Date Fish;
+    printf("请输入日期(年 月 日): ");
+    scanf("%d %d %d", &Fish.year, &Fish.month, &Fish.day);
+    if (FishNet(Fish)) {
+        printf("此人这天为打鱼");
+    }
+    else {
+        printf("此人这天为晒网");
+    }
+    return 0;
+}
+
+int FishNet (Date Fish) {
+    Date isFish = {1900, 1, 1};
+    while (isFish.year != Fish.year && isFish.month != Fish.month && isFish.day != Fish.day) {
+        if (isFish.day == 30) {
+            isFish.day = 1;
+            isFish.month += 1;
+        }
+        isFish.day += 1;
+    }
 }
