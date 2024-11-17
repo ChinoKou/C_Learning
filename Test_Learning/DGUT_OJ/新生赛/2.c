2b371ac14214f39e25c2661b5d969aebc2f1bdd9
ChinoKou <haohao927a@outlook.com>
2024-11-17 14:15:10 +0000

    2024年11月17日 22:15

--- Test_Learning/DGUT_OJ/新生赛/2.c
+++ Test_Learning/DGUT_OJ/新生赛/2.c
@@ -3,20 +3,21 @@
 
 int main () {
     int n;
-    long long arr[n], min;
+    long long min;
     scanf("%d", &n);
-    int* arr1 = malloc(4 * n);
+    long long* arr1 = malloc(8 * n);
+    long long* arr = malloc(8 * n);
     for (int i = 0; i < n; i++) {
         *(arr1 + i) = 0;
     }
     for (int i = 0; i < n; i++) {
-        scanf("%lld", &arr[i]);
-        *(arr1 + arr[i]) += 1;
+        scanf("%lld", (arr + i));
+        *(arr1 + *(arr + i)) += 1;
         if (i == 0) {
-            min = arr[0];
+            min = *arr;
         }
-        if (min > arr[i]) {
-            min = arr[i];
+        if (min > *(arr + i)) {
+            min = *(arr + i);
         }
         printf("%lld %lld\n", min, *(arr1 + min));
     }
