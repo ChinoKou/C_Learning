f0d9f923e4d89b8fd7ba2103c760a3cdf587d56c
ChinoKou <haohao927a@outlook.com>
2024-11-18 08:55:10 +0000

    2024年11月18日 16:55

--- Test_Learning/hw_lab/第四次实验/lab9.c
+++ Test_Learning/hw_lab/第四次实验/lab9.c
@@ -5,7 +5,7 @@ int main () {
     char s[1000];
     fgets(s, sizeof(s), stdin);
     for (int i = 0; i < strlen(s); i++) {
-        if (s[i] == 32) {
+        if (s[i] == 32 && s[i + 1] >= 97 && s[i + 1] <= 122) {
             s[i + 1] -= 32;
         }
     }
