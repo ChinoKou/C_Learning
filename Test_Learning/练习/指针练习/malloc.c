bb7b9fdc77d991dae5d3e1ad1089d42b9c2f064c
ChinoKou <haohao927a@outlook.com>
2024-11-18 07:57:11 +0000

    2024年11月18日 15:57

--- Test_Learning/练习/指针练习/malloc.c
+++ Test_Learning/练习/指针练习/malloc.c
@@ -4,7 +4,9 @@
 int main () {
     int n;
     scanf("%d", &n);
-    int* arr = (int*)malloc(n * sizeof(int));
+    int arr[n];
+    int* p = arr;
+    //int* arr = (int*)malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
         *(arr + i) = 0;
     }
--- Test_Learning/练习/指针练习/求数组最大值最小值.c
+++ Test_Learning/练习/指针练习/求数组最大值最小值.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-void GetMaxAndMin(int arr[], int len, int* max, int* min);
+void GetMaxAndMin(int* arr, int len, int* max, int* min);
 
 int main () {
     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int* max;
     int* min;
     int len = sizeof(arr) / sizeof(arr[0]);
-    GetMaxAndMin(arr, len, &max, &min);
+    GetMaxAndMin(arr, len, max, min);
     printf("%d %d", max, min);
 }
-void GetMaxAndMin(int arr[], int len, int* max, int* min) {
+void GetMaxAndMin(int* arr, int len, int* max, int* min) {
     *max = arr[0];
     *min = arr[0];
     for (int i = 0; i < len; i++) {
