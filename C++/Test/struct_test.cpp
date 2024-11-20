22c7d6e562e29986572e35af1880ec62e7e7625c
ChinoKou <haohao927a@outlook.com>
2024-11-20 13:43:23 +0000

    2024年11月20日 21:43

--- C++/Test/struct_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-typedef struct {
-    int year, month, day;
-} Data;
-int main() {
-    Data a = {1, 1, 1};
-    cout << a.year << " " << a.month << " " << a.day << endl;
-}
