fe3fc042ff092b85d6f294c929922d4caaaf0c94
ChinoKou <haohao927a@outlook.com>
2024-11-20 13:32:33 +0000

    2024年11月20日 21:32

---
+++ C++/Test/fish.cpp
@@ -0,0 +1,14 @@
+#include "fish.h"
+int FishNet (Date Fish) {
+    Date isFish = {1900, 1, 1};
+    int year = Fish.year - isFish.year;
+    int month = Fish.month - isFish.month;
+    int day = Fish.day - isFish.day;
+    int count = year * 12 * 30 + month * 30 + day;
+    if (count % 5 >= 0 && count % 5 <= 2) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
---
+++ C++/Test/fish.h
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+typedef struct {
+    int year;
+    int month;
+    int day;
+} Date;
+int FishNet (Date Fish);
