50850a3f37f1eb36770abdb84add622f60d9a224
ChinoKou <haohao927a@outlook.com>
2024-11-20 11:18:28 +0000

    2024年11月20日 19:18

--- C++/Test/test1.cpp
+++ C++/Test/test1.cpp
@@ -6,6 +6,8 @@ int main() {
     cout << "PRINYFA" << endl;
     string str = "WDANjfha";
     cout << str << endl;
+    bool flag = true;
+    cout << "size to bool " << sizeof(flag) << endl;
     system("pause");
     return 0;
 }
