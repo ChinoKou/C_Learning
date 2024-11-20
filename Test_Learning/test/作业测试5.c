09b2a3d3ca5c8e85d252d903f8dc786d1c5103dc
ChinoKou <haohao927a@outlook.com>
2024-11-20 10:46:21 +0000

    2024年11月20日 18:46

--- Test_Learning/test/作业测试5.c
+++ Test_Learning/test/作业测试5.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <string.h>
-int main()
-{
+int main() {
     char str[1000000];
     scanf("%s", str);
     int len = strlen(str);
     char* begin = str;
     char* end = str + len - 1;
     char temp;
-    while(begin <= end) {
-        temp = *begin;
-        *begin = *end;
-        *end = temp;
-        begin++;
-        end--;
+    for (int i = 0; i < (len / 2); i++) {
+        temp = *(begin + i);
+        *(begin + i) = *(end - i);
+        *(end - i) = temp;
     }
-
     printf("翻转后的字符串：%s",str);
+    return 0;
 }
