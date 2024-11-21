8acba791b067a6138687f3902807ed440255d8ff
ChinoKou <haohao927a@outlook.com>
2024-11-21 10:36:04 +0000

    2024年11月21日 18:36

---
+++ C++/struct/struct.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+typedef struct {
+    char name[100];
+    char stu_num[100];
+    double scores;
+} Stu;
+
+void sort (Stu* ArrStu);
+
+int main () {
+    Stu ArrStu[10];
+    for (int i = 0; i < 10; i++) {
+        printf("请输入第 %d 位学生的学号, 姓名和分数: ", i + 1);
+        scanf("%s %s %lf", ArrStu[i].stu_num, ArrStu[i].name, &ArrStu[i].scores);
+    }
+    sort(ArrStu);
+    printf("\n学生成绩如下: \n姓名\t 学号\t\t 成绩\t\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%s\t %s\t %lf\n", ArrStu[i].name, ArrStu[i].stu_num, ArrStu[i].scores);
+    }
+    return 0;
+}
+
+void sort (Stu* ArrStu) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = i; j < 10; j++) {
+            if (ArrStu[i].scores > ArrStu[j].scores) {
+                Stu temp = ArrStu[i];
+                ArrStu[i] = ArrStu[j];
+                ArrStu[j] = temp;
+            }
+        }
+    }
+}
