0bd36db88b7c49c58adcb441cb95c2734b68bd43
ChinoKou <haohao927a@outlook.com>
2024-11-21 06:28:05 +0000

    2024年11月21日 14:28

---
+++ Test_Learning/hw_lab/第四次实验/lab4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, arr[6][6], count = 0;
+    cout << "请输入一个正整数 n (1 <= n <= 6): ";
+    cin >> n;
+    cout << "请输入 " << n << " 阶方阵的元素: ";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        int max = arr[i][0];
+        for (int j = 0; j < n; j++) {
+            if (max < arr[i][j]) max = arr[i][j];
+        }
+        for (int j = 0; j < n; j++) {
+            if (max == arr[i][j]) {
+                int min = max;
+                for (int h =0 ; h < n; h++) {
+                    if (min > arr[h][j]) min = arr[h][j];
+                }
+                if (max == min) {
+                    cout << "该 " << n << " 阶方阵的鞍点下标为: " << i << ", " << j;
+                    return 0;
+                }
+            }
+        }
+    }
+    return 0;
+}
