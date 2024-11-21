c39679729ef2d6b2928b45942e3f275387a6a5af
ChinoKou <haohao927a@outlook.com>
2024-11-21 17:06:15 +0000

    2024年11月22日 01:06

--- Test_Learning/hw_lab/第四次实验/lab12.c
+++ Test_Learning/hw_lab/第四次实验/lab12.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-typedef struct {
+typedef struct{
     char grade;
     int order;
     double scores;
@@ -38,36 +38,36 @@ int main () {
 double Static (Stu* ArrStu, int* count_grade) {
     double sum = 0;
     for (int i = 0; i < 10; i++) {
-        if ((*ArrStu).marks.scores >= 90) {
-            (*ArrStu).marks.grade = 'A';
+        printf("%lf\n", ArrStu[i].marks.scores);
+        if ((*(ArrStu + i)).marks.scores >= 90) {
+            (*(ArrStu + i)).marks.grade = 'A';
             (*count_grade)++;
         }
-        else if ((*ArrStu).marks.scores >= 80 && (*ArrStu).marks.scores < 90) {
-            (*ArrStu).marks.grade = 'B';
+        else if ((*(ArrStu + i)).marks.scores >= 80 && (*(ArrStu + i)).marks.scores < 90) {
+            (*(ArrStu + i)).marks.grade = 'B';
             (*(count_grade + 1))++;
         }
-        else if ((*ArrStu).marks.scores >= 70 && (*ArrStu).marks.scores < 80) {
-            (*ArrStu).marks.grade = 'C';
+        else if ((*(ArrStu + i)).marks.scores >= 70 && (*(ArrStu + i)).marks.scores < 80) {
+            (*(ArrStu + i)).marks.grade = 'C';
             (*(count_grade + 2))++;
         }
-        else if ((*ArrStu).marks.scores >= 60 && (*ArrStu).marks.scores < 70) {
-            (*ArrStu).marks.grade = 'D';
+        else if ((*(ArrStu + i)).marks.scores >= 60 && (*(ArrStu + i)).marks.scores < 70) {
+            (*(ArrStu + i)).marks.grade = 'D';
             (*(count_grade + 3))++;
         }
-        else if ((*ArrStu).marks.scores >= 0 && (*ArrStu).marks.scores < 60) {
-            (*ArrStu).marks.grade = 'E';
+        else if ((*(ArrStu + i)).marks.scores >= 0 && (*(ArrStu + i)).marks.scores < 60) {
+            (*(ArrStu + i)).marks.grade = 'E';
             (*(count_grade + 4))++;
         }
-        sum += (*ArrStu).marks.scores;
-        ArrStu++;
+        sum += (*(ArrStu + i)).marks.scores;
     }
     //排序求学生成绩名次
     for (int i = 0; i < 10; i++) {
         for (int j = i; j < 10; j++) {
-            if ((*(ArrStu += i)).marks.scores < (*(ArrStu += j)).marks.scores) {
-                Stu temp = *(ArrStu += i);
-                *(ArrStu += i) = *(ArrStu += j);
-                *(ArrStu += j) = temp;
+            if ((*(ArrStu + i)).marks.scores < (*(ArrStu + j)).marks.scores) {
+                Stu temp = *(ArrStu + i);
+                *(ArrStu + i) = *(ArrStu + j);
+                *(ArrStu + j) = temp;
             }
         }
         (*(ArrStu + i)).marks.order = i + 1;
@@ -75,10 +75,10 @@ double Static (Stu* ArrStu, int* count_grade) {
     //根据原始排序还原顺序
     for (int i = 0; i < 10; i++) {
         for (int j = i; j < 10; j++) {
-            if ((*(ArrStu += i)).input_order > (*(ArrStu += j)).input_order) {
-                Stu temp = *(ArrStu += i);
-                *(ArrStu += i) = *(ArrStu += j);
-                *(ArrStu += j) = temp;
+            if ((*(ArrStu + i)).input_order > (*(ArrStu + j)).input_order) {
+                Stu temp = *(ArrStu + i);
+                *(ArrStu + i) = *(ArrStu + j);
+                *(ArrStu + j) = temp;
             }
         }
     }
