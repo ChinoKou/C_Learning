3f73a29943a39c19a56de0d4b5e61986d80d7249
ChinoKou <haohao927a@outlook.com>
2024-11-21 06:40:08 +0000

    2024年11月21日 14:40

--- Test_Learning/hw_lab/第四次实验/lab4.cpp
+++ Test_Learning/hw_lab/第四次实验/lab4.cpp
@@ -2,25 +2,24 @@
 using namespace std;
 
 int main() {
-    int n, arr[6][6], count = 0;
+    int n, arr[6][6], count = 0; 
     cout << "请输入一个正整数 n (1 <= n <= 6): ";
     cin >> n;
     cout << "请输入 " << n << " 阶方阵的元素: ";
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++) 
             cin >> *(*(arr + j) + i);
-        }
     }
     for (int i = 0; i < n; i++) {
-        int max = arr[i][0];
+        int max = *(*arr + i);
         for (int j = 0; j < n; j++) {
-            if (max < arr[i][j]) max = arr[i][j];
+            if (max < *(*(arr + j) + i)) max = *(*(arr + j) + i);
         }
         for (int j = 0; j < n; j++) {
-            if (max == arr[i][j]) {
+            if (max == *(*(arr + j) + i)) {
                 int min = max;
                 for (int h =0 ; h < n; h++) {
-                    if (min > arr[h][j]) min = arr[h][j];
+                    if (min > *(*(arr + j) + h)) min = *(*(arr + j) + h);
                 }
                 if (max == min) {
                     cout << "该 " << n << " 阶方阵的鞍点下标为: " << i << ", " << j;
