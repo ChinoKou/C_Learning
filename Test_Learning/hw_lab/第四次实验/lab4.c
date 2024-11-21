26f9d6ef8251442f42c193dc7640b94f85f679e8
ChinoKou <haohao927a@outlook.com>
2024-11-21 14:54:56 +0000

    2024年11月21日 22:54

--- Test_Learning/hw_lab/第四次实验/lab4.c
+++ Test_Learning/hw_lab/第四次实验/lab4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int n, arr[6][6], count = 0; 
+    int n, arr[6][6]; 
     printf("请输入一个正整数 n (1 <= n <= 6): ");
     scanf("%d", &n);
     printf("请输入 %d 阶方阵的元素: ", n);
@@ -18,7 +18,9 @@ int main() {
             if (max == *(*(arr + j) + i)) {
                 int min = max;
                 for (int h =0 ; h < n; h++) {
-                    if (min > *(*(arr + j) + h)) min = *(*(arr + j) + h);
+                    if (min > *(*(arr + j) + h)) {
+                        min = *(*(arr + j) + h);
+                    }
                 }
                 if (max == min) {
                     printf("该 %d 阶方阵的鞍点下标为: %d, %d", n, i, j);
