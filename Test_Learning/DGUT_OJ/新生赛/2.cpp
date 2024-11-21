b42a82a0c4f3fdbd53d42da328a062f23ed86542
ChinoKou <haohao927a@outlook.com>
2024-11-21 03:46:31 +0000

    2024年11月21日 11:46

--- Test_Learning/DGUT_OJ/新生赛/2.cpp
+++ Test_Learning/DGUT_OJ/新生赛/2.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 int main () {
-    int n, count = 0;
+    int n;
     int* arr1 = new int[1000000000000];
+    memset(arr1, 0, 1000000000000);
     cin >> n;
     long long min;
     long long* arr = new long long[n];
     for (int i = 0; i < n; i++) {
         cin >> *(arr + i);
-    }
-    min = *arr;
-    for (int i = 0; i < n; i++) {
+        *(arr1 + *(arr + i)) += 1;
+        if (i == 0) min = *arr;
         if (min > *(arr + i)) {
             min = *(arr + i);
-            count = 1;
         }
-        else if (min == *(arr + i)) {
-            count++;
-        }
-        printf("%lld %d\n", min, count);
+        printf("%lld %d\n", min, *(arr1 + min));
     }
     return 0;
 }
