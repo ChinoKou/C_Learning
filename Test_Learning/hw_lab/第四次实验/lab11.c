0942eb0f6062acf4345dbf3563480636cd12c84b
ChinoKou <haohao927a@outlook.com>
2024-11-27 10:12:28 +0000

    2024年11月27日 18:12

--- Test_Learning/hw_lab/第四次实验/lab11.c
+++ Test_Learning/hw_lab/第四次实验/lab11.c
@@ -25,11 +25,8 @@ int main () {
 }
 
 int FishNet (Date Fish) {
+    int count = 0;
     Date isFish = {1900, 1, 1};
-    int year = Fish.year - isFish.year;
-    int month = Fish.month - isFish.month;
-    int day = Fish.day - isFish.day;
-    int count = year * 12 * 30 + month * 30 + day;
     if (count % 5 >= 0 && count % 5 <= 2) {
         return 1;
     }
