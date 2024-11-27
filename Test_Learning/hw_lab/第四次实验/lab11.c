103cca6b28b1d70bb5c2ac605593a5427a43d716
ChinoKou <haohao927a@outlook.com>
2024-11-27 12:06:31 +0000

    Nov 27, 2024, 8:06 PM

--- Test_Learning/hw_lab/第四次实验/lab11.c
+++ Test_Learning/hw_lab/第四次实验/lab11.c
@@ -12,14 +12,15 @@ int is_bissextile (int year);
 
 int main () {
     Date Fish;
-    printf("请输入日期(年 月 日): ");
+    while (1) {
+    printf("\n请输入日期(年 月 日): ");
     scanf("%d %d %d", &Fish.year, &Fish.month, &Fish.day);
-    printf("若一个月只有30天\n");
     if (FishNet(Fish)) {
         printf("则此人这天为打鱼");
     }
     else {
         printf("则此人这天为晒网");
+    }
     }
     return 0;
 }
@@ -30,24 +31,20 @@ int FishNet (Date Fish) {
         days += 365 + is_bissextile(i);
     }
     for (int i = 1; i < Fish.month; i++) {
-        if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) {
-            days += 31;
+        if (i == 2) {
+            days += 28 + is_bissextile(Fish.year);
+            continue;
         }
-        else if (i == 4 || i == 6 || i == 9 || i == 11) {
+        if (i == 4 || i == 6 || i == 9 || i == 11) {
             days += 30;
         }
-        else if (i == 2) {
-            days += 28 + is_bissextile(Fish.year);
+        else {
+            days += 31;
         }
     }
-    if (days % 5 >= 0 && days % 5 <= 2) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
+    return (days % 5 >= 0 && days % 5 <= 2);
 }
 
 int is_bissextile (int year) {
-    return (!(year % 4) && (year % 100)) || !(year % 400);
+    return ((!(year % 4) && (year % 100)) || !(year % 400));
 }
