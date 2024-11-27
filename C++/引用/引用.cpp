e2e1767329253bb29a5c39643feec7297f1f22ad
ChinoKou <haohao927a@outlook.com>
2024-11-27 13:00:19 +0000

    Nov 27, 2024, 9:00 PM

---
+++ C++/引用/引用.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+long long dectobin (int n);
+
+int main () {
+    int n;
+    cout << "输入一个正整数 n : ";
+    cin >> n;
+    cout << "十进制数 " << n << " 的二进制数是 " << dectobin(n);
+    return 0;
+}
+
+long long dectobin (int& result) {
+    int n = result;
+    long long temp, result = 0;
+    for (int i = 0; n > 0; i++) {
+        temp = n % 2;
+        n /= 2;
+        result += temp * pow(10, i);
+    }
+    return result;
+}
