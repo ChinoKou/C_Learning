50c9c11b9bfcab5ad036d0921aaa9ef353dae0df
ChinoKou <haohao927a@outlook.com>
2024-11-27 12:27:58 +0000

    Nov 27, 2024, 8:27 PM

--- Test_Learning/hw_lab/第四次实验/lab11.c
+++ Test_Learning/hw_lab/第四次实验/lab11.c
@@ -33,7 +33,12 @@ int FishNet (Date Fish) {
             days += 28 + is_bissextile(Fish.year);
             continue;
         }
-        days += 30 + (i == 4 || i == 6 || i == 9 || i == 11);
+        if (i == 4 || i == 6 || i == 9 || i == 11) {
+            days += 30;
+        }
+        else {
+            days += 31;
+        }
     }
     return (days % 5 >= 0 && days % 5 <= 2);
 }
