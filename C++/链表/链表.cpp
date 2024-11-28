b70a205f278d2f3a04eaf5d7751a15377626208c
ChinoKou <haohao927a@outlook.com>
2024-11-28 14:53:21 +0000

    Nov 28, 2024, 10:53 PM

---
+++ C++/链表/链表.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+struct room {
+    int num;
+    char name[100];
+    room* next;
+};
+
+int main() {
+    room *head = NULL, *tmp = NULL, *tail = NULL;
+    tmp = new room({101, "a", NULL});
+}
