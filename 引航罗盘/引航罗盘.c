af9f8cf2083a605da8a9b82fdab016cb80b4a24a
ChinoKou <haohao927a@outlook.com>
2024-12-13 13:39:24 +0000

    2024年12月13日 21:39

--- 引航罗盘/引航罗盘.c
+++ 引航罗盘/引航罗盘.c
@@ -2,40 +2,40 @@
 #include <stdlib.h>
 #include <math.h>
 
-int getx(double(*A)[3], double* b, double* origin);
+int getx(double(*A)[3], double*x, double* b);
 
 int main() {
     int n = 3;
-    double PI = 3.14;
-    double OT = 1 / 3;
-    double A[3][3] = {
-        {0,      3.14, 3.14},
-        {-3.14, -3.14, 0},
-        {2.094,  0,    2.094}
-    };
+    double A[3][3];
+    A[0][0] = 0;            A[0][1] = 3.14;     A[0][2] = 3.14;
+    A[1][0] = -3.14;        A[1][1] = -3.14;    A[1][2] = 0;
+    A[2][0] = 2 * 3.14 / 3; A[2][1] = 0;        A[2][2] =  2 * 3.14 / 3;
     double b[3] = {6.28, 6.28, 6.28};
+    double temp_b[3];
     double origin[3] = {0, 3.14, 0};
-    int k[3];
-    for (k[1] = -5; k[1] <= 5; k[1]++) {
-        for (k[2] = -5; k[2] <= 5; k[2]++) {
-            for (k[3] = -5; k[3] <= 5; k[3]++) {
-                double temp_b[3];
+    int k[3] = {1, -1, 1};
+    double x[3];
+    for (k[0] = 1; k[0] <= 5; k[0]++) {
+        for (k[1] = -5; k[1] <= -1; k[1]++) {
+            for (k[2] = 1; k[2] <= 5; k[2]++) {
                 for (int i = 0; i < 3; i++) {
                     temp_b[i] = b[i];
                     temp_b[i] *= k[i];
+                    temp_b[i] -= origin[i];
                 }
-                getx(A, temp_b, origin);
-                for (int i = 0; i < 3; i++) {
-                    printf("K%d = %d\n", i + 1, k[i]);
+                if (getx(A, x, temp_b)) {
+                    for (int i = 0; i < 3; i++) {
+                        printf("X%d = %.1lf\n", i + 1, x[i]);
+                    }
+                    printf("\n");
                 }
             }
         }
     }
-    double x[n];
     return 0;
 }
 
-int getx(double(*A)[3], double* b, double* origin) {
+int getx(double(*A)[3], double*x, double* b) {
     double copy_A[3][3];
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
@@ -44,11 +44,11 @@ int getx(double(*A)[3], double* b, double* origin) {
     }
     for (int i = 0; i < 3; i++) {
         for (int k = i + 1; k < 3; k++) {
-            if (fabs(A[i][i]) < fabs(A[k][i])) {
+            if (abs(copy_A[i][i]) < abs(copy_A[k][i])) {
                 for (int j = 0; j < 3; j++) {
-                    double temp = A[i][j];
-                    A[i][j] = A[k][j];
-                    A[k][j] = temp;
+                    double temp = copy_A[i][j];
+                    copy_A[i][j] = copy_A[k][j];
+                    copy_A[k][j] = temp;
                 }
                 double temp = b[i];
                 b[i] = b[k];
@@ -56,21 +56,19 @@ int getx(double(*A)[3], double* b, double* origin) {
             }
         }
         for (int k = i + 1; k < 3; k++) {
-            double factor = A[k][i] / A[i][i];
+            double factor = copy_A[k][i] / copy_A[i][i];
             for (int j = i; j < 3; j++) {
-                A[k][j] -= factor * A[i][j];
+                copy_A[k][j] -= factor * copy_A[i][j];
             }
             b[k] -= factor * b[i];
-            origin[k] -= factor * origin[i];
         }
     }
-    double x[3];
     for (int i = 3 - 1; i >= 0; i--) {
         x[i] = b[i];
         for (int j = i + 1; j < 3; j++) {
-            x[i] -= A[i][j] * x[j];
+            x[i] -= copy_A[i][j] * x[j];
         }
-        x[i] /= A[i][i];
+        x[i] /= copy_A[i][i];
     }
-    return x[1] == floor(x[1]) && x[2] == floor(x[2]) && x[3] == floor(x[3]);
+    return  fabs(x[1] - round(x[1])) <= 0.2 && fabs(x[2] - round(x[2])) <= 0.2 && fabs(x[3] - round(x[3])) <= 0.2;
 }
