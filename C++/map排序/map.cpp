731982bb71226dcc9c6ecd29db1d38c2aea48a01
ChinoKou <haohao927a@outlook.com>
2025-03-17 11:42:15 +0000

    2025年3月17日 19:42

--- C++/map排序/map.cpp
+++ C++/map排序/map.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int main(){
     map<int, int> m;
+    int x;
     for (int i = 0; i < 10; i++){
-        cin >> m[i];
+        cin >> x;
+        m[x] = x;
     }
     for(auto& item : m) {
         cout << item.second << endl;
